7ffc4fdd4d3bd7d6c84bddfe692c877a3f22ceb2
Varsha Kushwah <varshakushwah711@gmail.com>
2024-11-08 21:53:58 +0530

    Add files via upload

---
+++ PrintFibonacci/script.js
@@ -0,0 +1,38 @@
+function fibonacci(n)
+{
+
+    let num = parseInt(document.getElementById("num").value);
+    let a = 0;
+    let b =1;
+
+    let arr = [];
+    if(isNaN(num))
+    {
+        document.getElementById("result").value = "Please enter a Number.";
+        return;
+    }
+
+    for(let i=0; i<=num; i++)
+    {
+        //document.getElementById("result").value = a;
+        arr.push(a);
+        let next = a+b;
+        a = b;
+        b = next;
+
+    }
+
+
+    document.getElementById("result").value = arr.join(", ");
+
+}
+
+function ResetField()
+{
+
+
+    document.getElementById("num").value = "";
+    document.getElementById("result").value = "";
+
+
+}
